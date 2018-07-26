072d6241473e4af399aec0883264bcc50bf10b73
Verbeke, Quinn <Quinn.Verbeke@unisys.com>
2018-07-26 10:55:27 -0400

    started to work on developing a model with k predictions, k predictions need to be correct.

--- elbowCalc.py
+++ elbowCalc.py
@@ -4,39 +4,37 @@ Created on Wed Jul 25 10:00:21 2018
 
 @author: PillaiAr
 """
+def get_k(myArray):
+    def ratioCalc(a,b,c,i):
+        top = b[1]-a[1]
+        bottom = c[1]-b[1]
+        ratio = top/bottom
+        return ratio
 
-myArray = [[1,3.4577],[2,1.7687],[3,0.88199],[4,0.7587],[5,0.6635],[6,0.6066],[7,0.50937],[8,0.43613],[9,0.33333]]
-
-def ratioCalc(a,b,c,i):
-    top = b[1]-a[1]
-    bottom = c[1]-b[1]
-    ratio = top/bottom
-    return ratio
-
-max = 0
+    max = 0
 
 
-n = len(myArray)
+    n = len(myArray)
 
 
-if(myArray[0][1] < myArray[-1][1]):
-    i = 1
-    while(i < n):
-        ratio = ratioCalc(myArray[i-1],myArray[i],myArray[i+1],i)
-        if(ratio >= max):
-            max = ratio
-        #else:
-         #   break
-        i+=1
+    if(myArray[0][1] < myArray[-1][1]):
+        i = 1
+        while(i < n):
+            ratio = ratioCalc(myArray[i-1],myArray[i],myArray[i+1],i)
+            if(ratio >= max):
+                max = ratio
+            #else:
+             #   break
+            i+=1
 
-else:
-    for i in range(len(myArray)-2,0,-1):
-        ratio = ratioCalc(myArray[i+1],myArray[i],myArray[i-1],i)
-        if(ratio >= max):
-            max = ratio
-        #else:
-        #    break
+    else:
+        for i in range(len(myArray)-2,0,-1):
+            ratio = ratioCalc(myArray[i+1],myArray[i],myArray[i-1],i)
+            if(ratio >= max):
+                max = ratio
+            #else:
+            #    break
 
-    
-elbow = myArray[i+1]
-print(elbow)
+        
+    elbow = myArray[i+1]
+    return elbow
--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
+import elbowCalc as ebc
+from scipy.spatial.distance import cdist
+
 
 
 """
@@ -9,20 +12,54 @@ pltdatax = [1,1,1,4,4,4]
 pltdatay = [2,4,0,2.5,4,0]
 
 """
-myary = np.random.randn(100,2)
 
-kmeans = KMeans(n_clusters=4, random_state=0).fit(myary)
-
-pltary = [[[],[]],[[],[]],[[],[]],[[],[]]]
-for i in range(len(myary)):
-    pltary[kmeans.labels_[i]][0].append(myary[i][0])
-    pltary[kmeans.labels_[i]][1].append(myary[i][1])
+class TrainedModel:
+    def __init__(self, myary):
+        self._distortions = []
+        for k in range(1,20):
+            self._kmeans = KMeans(n_clusters=k, random_state=0).fit(myary)
+            self._kmeans.fit(myary)
+            self._distortions.append([k,sum(np.min(cdist(myary, self._kmeans.cluster_centers_, 'euclidean'), axis=1))/myary.shape[0]])
+        print(ebc.get_k(self._distortions))
+        
+    def classify_point(self, vec):
+        return self._kmeans.predict(vec)
+    def retrain(self, myary):
+        self._kmeans = KMeans(n_clusters=2, random_state=0).fit(myary)
+    def get_labels(self):
+        return self._kmeans.labels_
     
-plt.scatter(pltary[0][0],pltary[0][1], color="r")
-plt.scatter(pltary[1][0],pltary[1][1],color="b")
-plt.scatter(pltary[2][0],pltary[2][1],color="g")
-plt.scatter(pltary[3][0],pltary[3][1],color="c")
-plt.show()
 
-print(kmeans.labels_)
-print(myary)
+
+myary1 = np.random.randn(100,4)
+for i in range(100):
+    myary1[i][3]+=100
+myary2 = np.random.randn(100,4)
+for i in range(100):
+    myary2[i][3]-=100
+myary3 = np.random.randn(100,4)
+myary4 = np.random.randn(100,4)
+for i in range(100):
+    myary4[i][2]+=100
+myary5 = np.random.randn(100,4)
+for i in range(100):
+    myary5[i][2]-=100
+myary=np.concatenate((myary1, myary2,myary3,myary4,myary5), axis=0)
+
+thing = TrainedModel(myary)
+#print(thing.classify_point([[0,0,0,-2]]))
+#print(thing.get_labels())
+
+
+#pltary = [[[],[]],[[],[]]]#,[[],[]],[[],[]]]
+pltary=[[],[]]
+for i in range(len(thing._distortions)):
+    pltary[0].append(thing._distortions[i][0])
+    pltary[1].append(thing._distortions[i][1])
+    
+plt.scatter(pltary[0],pltary[1], color="r")
+#plt.scatter(pltary[1][0],pltary[1][1],color="b")
+#plt.scatter(pltary[2][0],pltary[2][1],color="g")
+#plt.scatter(pltary[3][0],pltary[3][1],color="c")
+plt.show()
+#print(kmeans.labels_)
